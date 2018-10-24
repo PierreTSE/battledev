38fe9646c696cbb5b89b90dc0fdb52fa4cd3b085
PierreTSE <34136165+PierreTSE@users.noreply.github.com>
2018-10-24 19:07:25 +0200

    string split

--- Fonctions_string.hpp
+++ Fonctions_string.hpp
@@ -25,5 +25,19 @@ std::vector<std::string> separe_mots(std::string str)
 	return v_out;
 }
 
+std::vector<std::string> split(const std::string& str, const std::string& sep = " ")
+{
+	std::vector<std::string> out;
+	size_t debut = 0;
+	size_t fin = str.find(sep);
+    while(debut <= str.length())
+    {
+		out.push_back(str.substr(debut, fin - debut));
+		debut = fin != std::string::npos ? fin + sep.length() : fin;
+		fin = str.find(sep, debut);
+    }
+	return out;
+}
+
 #endif // !FONCTIONS_STRING_H
 
